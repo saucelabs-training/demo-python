d0714d93d05adefa90258d2f09cca4ae946cbba3
joshgrant <joshua.m.grant@gmail.com>
2020-10-08 14:08:18 -0400

    created legacyTO directory for older style RDC tests

--- appium-examples/pytest/rdc/legacyTO/android/tests/test_invald_login.py
+++ appium-examples/pytest/rdc/legacyTO/android/tests/test_invald_login.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def test_blank_credentials(android_driver):
     android_driver.find_element_by_accessibility_id("test-Username").send_keys("")
     android_driver.find_element_by_accessibility_id("test-Password").send_keys("")
---
+++ appium-examples/pytest/rdc/legacyTO/ios/tests/test_valid_login.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+
+def test_standard_user(ios_driver):
+    sleep(5)
+    ios_driver.find_element_by_accessibility_id("test-Username").send_keys("standard_user")
+    ios_driver.find_element_by_accessibility_id("test-Password").send_keys("secret_sauce")
+    ios_driver.find_element_by_accessibility_id("test-LOGIN").click()
+
+    assert ios_driver.find_element_by_accessibility_id("test-PRODUCTS").is_displayed()
