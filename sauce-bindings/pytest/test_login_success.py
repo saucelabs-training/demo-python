43facd40c7d4a6132016957ae288e6a65c4a7434
joshgrant <joshua.m.grant@gmail.com>
2021-03-03 15:17:18 -0500

    updated sauce bindings examples

--- sauce-bindings/pytest/test_login_success.py
+++ sauce-bindings/pytest/test_login_success.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_valid_crentials_login(driver):
-    driver.get('http://www.saucedemo.com')
+    driver.get('http://www.saucedemo.com/v1')
 
     driver.find_element_by_id('user-name').send_keys('standard_user')
     driver.find_element_by_id('password').send_keys('secret_sauce')
