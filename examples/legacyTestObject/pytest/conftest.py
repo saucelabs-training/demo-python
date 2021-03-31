40e12b002e90a8a3042f8d5227fa699ed65fe5fc
joshgrant <joshua.m.grant@gmail.com>
2021-03-31 15:11:38 -0400

    more restructuring

--- examples/legacyTestObject/pytest/conftest.py
+++ examples/legacyTestObject/pytest/conftest.py
@@ -14,56 +14,6 @@ def pytest_addoption(parser):
 def data_center(request):
     return request.config.getoption('--dc')
 
-
-@pytest.fixture
-def ios_up_driver(request, data_center):
-    caps = {
-        'username': os.environ['SAUCE_USERNAME'],
-        'accessKey': os.environ['SAUCE_ACCESS_KEY'],
-        'deviceName': 'iPhone.*',
-        'platformName': 'iOS',
-        'name': request.node.name,
-        'app': 'storage:filename=iOS.RealDevice.SauceLabs.Mobile.Sample.app.2.2.1.ipa'
-    }
-
-    if data_center and data_center.lower() == 'eu':
-        sauce_url = "http://ondemand.eu-central-1.saucelabs.com/wd/hub"
-    else:   
-        sauce_url = "http://ondemand.us-west-1.saucelabs.com/wd/hub"
-
-    driver = webdriver.Remote(sauce_url, desired_capabilities=caps)
-    yield driver
-    sauce_result = "failed" if request.node.rep_call.failed else "passed"
-    driver.execute_script("sauce:job-result={}".format(sauce_result))
-    driver.quit()
-
-
-@pytest.fixture
-def ios_simulator(request, data_center):
-    caps = {
-        'username': os.environ['SAUCE_USERNAME'],
-        'accessKey': os.environ['SAUCE_ACCESS_KEY'],
-        'deviceName': 'iPhone XS Simulator',
-        'appiumVersion': '1.17.1',
-        'platformName': 'iOS',
-        'platformVersion': "13.4",
-        'deviceOrientation': "portrait",
-        'name': request.node.name,
-        'app': 'storage:filename=iOS.Simulator.SauceLabs.Mobile.Sample.app.2.7.0.zip'
-    }
-
-    if data_center and data_center.lower() == 'eu':
-        sauce_url = "http://ondemand.eu-central-1.saucelabs.com/wd/hub"
-    else:   
-        sauce_url = "http://ondemand.us-west-1.saucelabs.com/wd/hub"
-
-    driver = webdriver.Remote(sauce_url, desired_capabilities=caps)
-    yield driver
-    sauce_result = "failed" if request.node.rep_call.failed else "passed"
-    driver.execute_script("sauce:job-result={}".format(sauce_result))
-    driver.quit()
-
-
 @pytest.fixture
 def ios_to_driver(request, data_center):
     
@@ -129,57 +79,6 @@ def android_to_driver(request, data_center):
         raise WebDriverException("Never created!")
 
     yield driver
-    driver.quit()
-
-
-@pytest.fixture
-def android_up_driver(request, data_center):
-    caps = {
-        'username': os.environ['SAUCE_USERNAME'],
-        'accessKey': os.environ['SAUCE_ACCESS_KEY'],
-        'deviceName': 'Google.*',
-        'platformName': 'Android',
-        'name': request.node.name,
-        'app': 'storage:filename=Android.SauceLabs.Mobile.Sample.app.2.3.0.apk'
-    }
-
-    if data_center and data_center.lower() == 'eu':
-        sauce_url = 'http://ondemand.eu-central-1.saucelabs.com/wd/hub'
-    else:
-        sauce_url = 'http://ondemand.us-west-1.saucelabs.com/wd/hub'
-
-    driver = webdriver.Remote(sauce_url, desired_capabilities=caps)
-    yield driver
-    sauce_result = "failed" if request.node.rep_call.failed else "passed"
-    driver.execute_script("sauce:job-result={}".format(sauce_result))
-    driver.quit()
-
-
-@pytest.fixture
-def android_emulator(request, data_center):
-    caps = {
-        'username': os.environ['SAUCE_USERNAME'],
-        'accessKey': os.environ['SAUCE_ACCESS_KEY'],
-        'deviceName': 'Android GoogleAPI Emulator',
-        'platformName': 'Android',
-        'platformVersion': '10.0',
-        'deviceOrientation': 'portrait',
-        'name': request.node.name,
-        'appiumVersion': '1.17.1',
-        'appWaitActivity': 'com.swaglabsmobileapp.MainActivity',
-        'app': 'storage:filename=Android.SauceLabs.Mobile.Sample.app.2.3.0.apk'
-
-    }
-
-    if data_center and data_center.lower() == 'eu':
-        sauce_url = 'http://ondemand.eu-central-1.saucelabs.com/wd/hub'
-    else:
-        sauce_url = 'http://ondemand.us-west-1.saucelabs.com/wd/hub'
-
-    driver = webdriver.Remote(sauce_url, desired_capabilities=caps)
-    yield driver
-    sauce_result = "failed" if request.node.rep_call.failed else "passed"
-    driver.execute_script("sauce:job-result={}".format(sauce_result))
     driver.quit()
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
