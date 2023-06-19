1f692432d25663bda52243359c41479b242a7d7b
Daniel Graham <daniel.graham@saucelabs.com>
2023-06-19 12:00:17 -0700

    Add Gitpod selenium test

--- examples/gitpod/test_selenium.py
+++ examples/gitpod/test_selenium.py
@@ -1,14 +1,32 @@
-from saucebindings.session import SauceSession
-from saucebindings.session import SauceOptions
+import os
 
-def test_from_gitpod(driver):
-    options = SauceOptions('firefox')
-    options.browser_version = '106'
-    options.platform_name = 'Windows 11'
-    options.build = '<your build id>'
-    options.name = '<your test name>'
-    sauce = SauceSession(options=options)
-    sauce.start()
-    title = sauce.title
-    assert "" in title
-    
+from selenium import webdriver
+
+class TestSelenium(object):
+
+    def test_selenium_on_gitpod(self):
+        test_name = os.environ.get('PYTEST_CURRENT_TEST').split(':')[-1].split(' ')[0]
+        gitpod_workspace_id = os.environ.get('GITPOD_WORKSPACE_ID', None)
+        region = os.environ.get('REGION', 'us-west-1')
+        if gitpod_workspace_id:
+            tags = ['gitpod']
+        else:
+            tags = []
+        capabilities = {
+            'browserName': os.environ.get('BROWSER_NAME', 'chrome'),
+            'browserVersion': os.environ.get('BROWSER_VERSION', None),
+            'platformName': os.environ.get('PLATFORM_NAME', None),
+            'sauce:options': {
+                'username': os.environ["SAUCE_USERNAME"],
+                'accesskey': os.environ["SAUCE_ACCESS_KEY"],
+                'build': os.environ.get('BUILD', None),
+                'name': os.environ.get('JOB_NAME', test_name),
+                'tags': ','.join(tags),
+            }
+        }
+        sauce_url = f'https://ondemand.{region}.saucelabs.com/wd/hub'
+        driver = webdriver.Remote(sauce_url, capabilities)
+
+        driver.get('https://www.saucedemo.com/')
+
+        driver.quit()
