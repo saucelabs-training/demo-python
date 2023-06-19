554913ad8ae9071b98d899b79e94d07b61b35b55
Daniel Graham <daniel.graham@saucelabs.com>
2023-06-19 13:03:32 -0700

    add Appium scripts too

--- examples/gitpod/test_selenium.py
+++ examples/gitpod/test_selenium.py
@@ -1,10 +1,11 @@
 import os
+import time
 
 from selenium import webdriver
 
 class TestSelenium(object):
 
-    def test_selenium_on_gitpod(self):
+    def test_selenium_with_gitpod(self):
         test_name = os.environ.get('PYTEST_CURRENT_TEST').split(':')[-1].split(' ')[0]
         gitpod_workspace_id = os.environ.get('GITPOD_WORKSPACE_ID', None)
         region = os.environ.get('REGION', 'us-west-1')
@@ -21,12 +22,19 @@ class TestSelenium(object):
                 'accesskey': os.environ["SAUCE_ACCESS_KEY"],
                 'build': os.environ.get('BUILD', None),
                 'name': os.environ.get('JOB_NAME', test_name),
-                'tags': ','.join(tags),
+                'tags': tags,
             }
         }
-        sauce_url = f'https://ondemand.{region}.saucelabs.com/wd/hub'
+        sauce_url = f'https://ondemand.{region}.saucelabs.com:443/wd/hub'
         driver = webdriver.Remote(sauce_url, capabilities)
 
+        # Add commands and assertions here
         driver.get('https://www.saucedemo.com/')
+        time.sleep(5)
+
+        # Report pass/fail to Sauce Labs
+        # sauce_result = "failed"
+        sauce_result = "passed"
+        driver.execute_script("sauce:job-result={}".format(sauce_result))
 
         driver.quit()
